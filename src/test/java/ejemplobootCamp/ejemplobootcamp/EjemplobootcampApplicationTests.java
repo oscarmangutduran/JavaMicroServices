65844df689697942f6f33a85fbb25cb3fa36659a
Oscar Mangut <mangutduranoscar@gmail.com>
2023-04-18 14:27:19 +0200

    reto3
    microservicios

---
+++ src/test/java/ejemplobootCamp/ejemplobootcamp/EjemplobootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package ejemplobootCamp.ejemplobootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemplobootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
